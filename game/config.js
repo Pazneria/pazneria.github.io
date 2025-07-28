724912cf5c248ccfd840826b74023cc73f29248b
Pazneria <j.moren.awesome@gmail.com>
2025-07-28 00:13:04 -0700

    Add config.js with shared constants (tile size, map dimensions, walk speed and colors)

---
+++ game/config.js
@@ -0,0 +1,11 @@
+export default {
+  tileSize: 32,
+  mapWidth: 20,
+  mapHeight: 15,
+  walkSpeed: 200,
+  backgroundColor: '#0b0f27',
+  resourceColors: {
+    ore: 0x8888aa,
+    scrap: 0xaa8844
+  }
+};
